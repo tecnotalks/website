7e1a6cd09b94155732bcdbcab2e4ec220181fdf6
Maeda <andherson.maeda@personaltek.com>
2013-05-23 01:50:33 -0300

    Commit inicial

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,7 @@ public class Application extends Controller {
   
     public static Result index() {
         return ok(index.render("Your new application is ready."));
-    }
+    		//return ok(index.render("Principal"));
+	}
   
 }
