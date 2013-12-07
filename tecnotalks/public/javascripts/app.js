7ae031336f3af72e004f922b1a987ac905f3d18c
Paulo Eduardo Kopzinski Mello <eduardo.geleia@gmail.com>
2013-12-07 15:18:38 -0200

    angular bombando

---
+++ tecnotalks/public/javascripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('tecnotalksSite', [
+  'ngRoute',
+  'tecnotalksSite.services',
+  'tecnotalksSite.controllers'
+]).
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.when('/agenda', {templateUrl: '/views/agenda.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/sugestoes', {templateUrl: '/views/sugestoes.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/'});
+
+  $locationProvider.html5Mode(true);
+
+}]);
---
+++ tecnotalks/public/javascripts/services.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('tecnotalksSite.services', []).
+  factory('MyService', [function() {
+
+  }]);
