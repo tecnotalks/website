eb79a480a98e354055ae495b80004343ad080879
oswagner <wagner.santos@esales.com.br>
2013-12-07 17:28:59 -0200

    interface inicial e dashboard.

--- tecnotalks/public/javascripts/app.js
+++ tecnotalks/public/javascripts/app.js
@@ -8,8 +8,10 @@ angular.module('tecnotalksSite', [
   'tecnotalksSite.controllers'
 ]).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {templateUrl: '/views/dashboard.html'});
   $routeProvider.when('/agenda', {templateUrl: '/views/agenda.html', controller: 'MyCtrl1'});
   $routeProvider.when('/sugestoes', {templateUrl: '/views/sugestoes.html', controller: 'MyCtrl2'});
+
   $routeProvider.otherwise({redirectTo: '/'});
 
   $locationProvider.html5Mode(true);
