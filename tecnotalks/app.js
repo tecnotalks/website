301d1afcb07d502e5b3bac6762d3ca633e8a15a6
Marcos Vinicius <marcos@kallx.it>
2013-12-07 17:46:27 -0200

    Salvando usuario do registro

--- tecnotalks/app.js
+++ tecnotalks/app.js
@@ -9,6 +9,7 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var EventProvider = require("./eventprovider-mongodb").EventProvider;
+var UserProvider = require("./userprovider-mongodb").UserProvider;
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.get('/', function(req, res) {
 });
 
 var eventProvider = new EventProvider('localhost', 27017);
+var userProvider = new UserProvider('localhost', 27017);
 
 // development only
 if ('development' == app.get('env')) {
@@ -56,6 +58,19 @@ app.get('/services/events/list', function(req, res){
         	res.send(docs)
         });
     });
+
+/* Salvar usuaro */
+app.post('/services/user/save', function(req, res){
+        userProvider.save(req.body, function(users){res.send(users);});
+    });
+
+/* listar usuario */
+app.get('/services/user/list', function(req, res){
+    userProvider.findAll( function(error,docs){
+            res.send(docs)
+        });
+    });
+
 
 
 http.createServer(app).listen(app.get('port'), function(){
